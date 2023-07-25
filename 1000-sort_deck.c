8ba47bea945cd8a782518acb2e1dc027939086c9
calebrori <32507245+calebrori@users.noreply.github.com>
2023-07-25 22:46:18 +0300

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -3,6 +3,8 @@
  * _strcmp - compares two strings.
  * @s1: first string to be compared.
  * @s2: second string to be compared.
+ *
+ * Return: Positive byte difference if s1 > s2
  */
 int _strcmp(const char *s1, const char *s2)
 {
